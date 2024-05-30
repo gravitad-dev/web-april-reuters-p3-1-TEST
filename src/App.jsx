b30c0db6d249980d30114cdc0fc76e66b67eeee9
silvak <silvak.jeg@gmail.com>
2024-05-30 19:40:53 -0400

    test 6

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
       
       <iframe
         class="aplication"
-        src="https://news-proxy-eta.vercel.app"
+        src="https://news-proxy-three.vercel.app"
         width="100"
         height="100"
         frameborder="0"
