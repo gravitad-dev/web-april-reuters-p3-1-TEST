a64ddb6946104e83a76bd9285daf5e294618b1ba
silvak <silvak.jeg@gmail.com>
2024-05-30 18:17:38 -0400

    test iframe

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='frameContainer'>
+      <iframe src="https://news-proxy-eta.vercel.app/" frameborder="0" ></iframe>
+    </div>
+  )
+}
+
+export default App
