172aadea0ae48ba2e7caffc3ed519b245f89dc4c
silvak <silvak.jeg@gmail.com>
2024-05-30 19:10:16 -0400

    test 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+//
 function App() {
   return (
     <div className='frameContainer'>
-      <iframe src="https://news-proxy-eta.vercel.app/" frameborder="0" ></iframe>
+      <div>aaa</div>
+      <iframe
+        class="aplication"
+        src="https://news-proxy-eta.vercel.app"
+        width="100"
+        height="100"
+        frameborder="0"
+      ></iframe>
     </div>
   )
 }
